2f1350e6a1ac0f80ca7b047953652653cd9706d9
mhuang <mhuang_email@163.com>
2018-04-12 14:30:21 +0800

    修改bug！

--- interchan-auth-common/src/main/java/com/petecat/interchan/auth/common/AuthFilter.java
+++ interchan-auth-common/src/main/java/com/petecat/interchan/auth/common/AuthFilter.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.alibaba.fastjson.JSON;
+import com.mhuang.common.util.DataUtils;
 import com.mhuang.common.webmvc.WebRequestHeader;
 import com.petecat.interchan.core.constans.Global;
 import com.petecat.interchan.core.exception.BusinessException;
@@ -49,41 +50,48 @@ public class AuthFilter implements Filter{
 	public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		if(req instanceof HttpServletRequest) {
-			HttpServletRequest request = (HttpServletRequest) req;
-			WebRequestHeader httpRequest = new WebRequestHeader(request);
-			HttpServletResponse httpResponse = (HttpServletResponse) response;
-			logger.info("请求的URL:{},请求类型:{}",request.getRequestURL().toString(),request.getMethod());
-			GlobalHeader globalHeader = new GlobalHeader();
-			globalHeader.setIp(IpUtils.getIp(request));
-			Jwt jwt = getMapper(Jwt.class, request);
-			String auth = request.getHeader(jwt.getType());
-			if(StringUtils.isBlank(auth)){
-				httpRequest.putHeader(Global.GLOBAL_HEADER, JSON.toJSONString(globalHeader));
-			}else if(StringUtils.length(auth) > TOKEN_HEADER_LENGTH){
-				String token = StringUtils.substringAfter(auth, jwt.getHeaderName());
-				try {
-					Claims claims = JwtHelper.parseJWT(token, jwt.getBase64Secret());
-					if(claims != null){
-						globalHeader.setToken(token);
-						globalHeader.setType((String)claims.get("type"));
-						globalHeader.setCompanyId((String)claims.get(Global.COMPANY_ID));
-						globalHeader.setUserId((String) claims.get(Global.USERID));
-						httpRequest.putHeader(Global.GLOBAL_HEADER, JSON.toJSONString(globalHeader));
-					}else{
-						throw new BusinessException(Result.TOKEN_IS_VALID,JSON.toJSONString(Result.tokenValid()));
+			
+			try{
+				HttpServletRequest request = (HttpServletRequest) req;
+				WebRequestHeader httpRequest = new WebRequestHeader(request);
+				HttpServletResponse httpResponse = (HttpServletResponse) response;
+				logger.info("请求的URL:{},请求类型:{}",request.getRequestURL().toString(),request.getMethod());
+				GlobalHeader globalHeader = new GlobalHeader();
+				globalHeader.setIp(IpUtils.getIp(request));
+				Jwt jwt = getMapper(Jwt.class, request);
+				String auth = request.getHeader(jwt.getType());
+				if(StringUtils.isBlank(auth)){
+					httpRequest.putHeader(Global.GLOBAL_HEADER, JSON.toJSONString(globalHeader));
+				}else if(StringUtils.length(auth) > TOKEN_HEADER_LENGTH){
+					String token = StringUtils.substringAfter(auth, jwt.getHeaderName());
+					try {
+						Claims claims = JwtHelper.parseJWT(token, jwt.getBase64Secret());
+						if(claims != null){
+							globalHeader.setToken(token);
+							globalHeader.setType((String)claims.get("type"));
+							globalHeader.setCompanyId((String)claims.get(Global.COMPANY_ID));
+							globalHeader.setUserId((String) claims.get(Global.USERID));
+							httpRequest.putHeader(Global.GLOBAL_HEADER, JSON.toJSONString(globalHeader));
+						}else{
+							throw new BusinessException(Result.TOKEN_IS_VALID,JSON.toJSONString(Result.tokenValid()));
+						}
+					} catch(ExpiredJwtException e){
+	                	logger.error("token已过期:{}",e);
+	                	throw new BusinessException(Result.TOKEN_EXPIRED,JSON.toJSONString(Result.tokenExpired()));  
+	                } catch (Exception e) {
+	                	logger.error("token异常:{}",e);
+	                	throw new BusinessException(Result.TOKEN_IS_VALID,JSON.toJSONString(Result.tokenValid()));  
 					}
-				} catch(ExpiredJwtException e){
-                	logger.error("token已过期:{}",e);
-                	throw new BusinessException(Result.TOKEN_EXPIRED,JSON.toJSONString(Result.tokenExpired()));  
-                } catch (Exception e) {
-                	logger.error("token异常:{}",e);
-                	throw new BusinessException(Result.TOKEN_IS_VALID,JSON.toJSONString(Result.tokenValid()));  
+				}else{
+					logger.error("token:{}无效，长度不一致",auth);
+					throw new BusinessException(Result.TOKEN_IS_VALID,JSON.toJSONString(Result.tokenValid()));   
 				}
-			}else{
-				logger.error("token:{}无效，长度不一致",auth);
-				throw new BusinessException(Result.TOKEN_IS_VALID,JSON.toJSONString(Result.tokenValid()));   
+				chain.doFilter(httpRequest, httpResponse);
+			}catch(BusinessException e){
+				Result<?> result = DataUtils.copyTo(e, Result.class);
+				response.getWriter().write(JSON.toJSONString(result));
 			}
-			chain.doFilter(httpRequest, httpResponse);
+			
 		}
 	}
 
