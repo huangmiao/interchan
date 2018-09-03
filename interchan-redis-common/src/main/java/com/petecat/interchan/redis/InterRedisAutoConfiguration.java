acd30bce3da6e638f3aac9c9e0bebfb4ffce5a58
mhuang <mhuang_email@163.com>
2018-09-03 10:30:38 +0800

    删除无用的引入！

--- interchan-redis-common/src/main/java/com/petecat/interchan/redis/InterRedisAutoConfiguration.java
+++ interchan-redis-common/src/main/java/com/petecat/interchan/redis/InterRedisAutoConfiguration.java
@@ -1,6 +1,5 @@
 package com.petecat.interchan.redis;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
